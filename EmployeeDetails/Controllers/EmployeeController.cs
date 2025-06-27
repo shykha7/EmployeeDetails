7676ba734cd0c3f97de1b600fbcad04488b2f4ef
shaikha <shyyykhaaa@gmail.com>
2025-06-27 16:28:38 +0530

    Add project files.

---
+++ EmployeeDetails/Controllers/EmployeeController.cs
@@ -0,0 +1,31 @@
+﻿using EmployeeDetails.Models;
+using EmployeeDetails.Services;
+using Microsoft.AspNetCore.Mvc;
+namespace EmployeeDetails.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EmployeeController : ControllerBase
+    {
+        private readonly IEmployeeService _employee;
+
+        public EmployeeController(IEmployeeService employee) 
+        { 
+            _employee = employee;
+        }
+
+        [HttpPost]
+        public IActionResult AddEmployee([FromBody] Employee employee)
+        {
+            var addedEmployee = employee;
+            return CreatedAtAction(nameof(AddEmployee), null, addedEmployee);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult UpdatedEmployee(int id, [FromBody] Employee employee)
+        {
+            var updatedemployee = employee;
+            return CreatedAtAction(nameof(UpdatedEmployee), new { id = updatedemployee.Id}, updatedemployee);
+        }
+    }
+}
---
+++ EmployeeDetails/Services/EmployeeService.cs
@@ -0,0 +1,29 @@
+﻿using EmployeeDetails.Models;
+namespace EmployeeDetails.Services
+{
+    public interface IEmployeeService
+    {
+        Employee AddEmployee(Employee employee);
+        Employee UpdatedEmployee(int id, Employee employee);
+    }
+
+    public class EmployeeService : IEmployeeService
+    {
+        private   List<Employee> employees;
+        public Employee AddEmployee(Employee employee)
+        {
+            employees.Add(employee);
+            return (employee);
+        }
+
+        public Employee UpdatedEmployee(int id,Employee employee)
+        {
+            if(id == null) { return null; }
+            var updatedemplyee = employees.FirstOrDefault(e => e.Id == id);
+            employee.Name = updatedemplyee.Name;
+            employee.Salary = updatedemplyee.Salary;
+            return (employee);
+
+        }
+    }
+}
